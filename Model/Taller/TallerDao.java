5fc11d0b5c5efce6e06103eb5520af662fe718e5
eJwtzMtuwjAQQNF/mbU3gQTX3lWwoKKbPtQHG8uxJ2DiZNxxAkSIf6+Esr3SuTewzmHOn9RiDxoKpbDw3staVVhLW7lSNaUqlqsGq5Vf2EVZL6V9AgGJ6Rw88kxv88kM8+pwJLNZ/8qX3dur3H7/HDt630+bazyHjxP+nXZfBfb0XIGABzHDlBA01GgZGQRkR49gfRd6ncY6BmdanASj9Zr4IBgTiTEja+xsiOJC3DaRLiAArykwZmMH0P0Y4/3+D8hIUNs= <zurojasco@est.utn.ac.cr>
2024-11-27 14:43:24 -0600

    Se crea la clase Mapper
    para poder trabajar con los mapper

---
+++ Model/Taller/TallerDao.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model.Taller;
+
+import Model.Dao.Dao;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Student
+ */
+public class TallerDao implements Dao<Taller>{
+
+    @Override
+    public boolean crear(Taller objeto) {
+        
+    }
+
+    @Override
+    public Taller leer(Object objeto) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public ArrayList leerTodo(Object objeto) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
