12ad0a77f0e507f035a7222e3e4eb631e2d1ce27
eJwtzMtuwjAQQNF/mbU3gQTX3lWwoKKbPtQHG8uxJ2DiZNxxAkSIf6+Esr3SuTewzmHOn9RiDxoKpbDw3staVVhLW7lSNaUqlqsGq5Vf2EVZL6V9AgGJ6Rw88kxv88kM8+pwJLNZ/8qX3dur3H7/HDt630+bazyHjxP+nXZfBfb0XIGABzHDlBA01GgZGQRkR49gfRd6ncY6BmdanASj9Zr4IBgTiTEja+xsiOJC3DaRLiAArykwZmMH0P0Y4/3+D8hIUNs= <zurojasco@est.utn.ac.cr>
2024-11-27 16:48:53 -0600

    Clase Taller Dao

--- Model/Taller/TallerDao.java
+++ Model/Taller/TallerDao.java
@@ -5,14 +5,25 @@
 package Model.Taller;
 
 import Model.Dao.Dao;
+import Model.Participantes.ParticipanteDTO;
+import java.sql.SQLException;
 import java.util.ArrayList;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.ResultSet;
+import java.sql.PreparedStatement;
+
 
 /**
  *
  * @author Student
  */
 public class TallerDao implements Dao<Taller> {
+ protected Connection connection;
 
+    public TallerDao(Connection connection) {
+        this.connection = connection;
+    }
     @Override
     public boolean crear(Taller objeto) {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
@@ -21,16 +32,42 @@ public class TallerDao implements Dao<Taller> {
 
     @Override
     public Taller leer(Object objeto)throws SQLException{
-        if (objeto == null){
-            return false;
+        if (objeto == null);
+        String query = "Call tallerRead(?)";
+        try ( PreparedStatement stmt = connection.prepareStatement(query)) {
+            stmt.setInt(1, Integer.parseInt(String.valueOf(objeto)));
+            try(ResultSet rs = stmt.executeQuery()){
+                if(rs.next()){
+                    return new TallerDTO(
+                            rs.getInt(1), 
+                            rs.getString(2), 
+                            rs.getDate(3), 
+                            rs.getString(4),
+                            rs.getInt(5));
+                }
+            }
         }
-        String query = "Call "
-        
+        return null;
+         
     }
 
     @Override
-    public ArrayList leerTodo() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    public ArrayList leerTodo()throws SQLException {
+     String query = "Call participantesRead(?)";
+        ArrayList<TallerDTO> list = new ArrayList<>();
+        try ( PreparedStatement stmt = connection.prepareStatement(query)) {
+            try(ResultSet rs = stmt.executeQuery()){
+                while(rs.next()){
+                    list.add(new TallerDTO(
+                             rs.getInt(1), 
+                            rs.getString(2), 
+                            rs.getDate(3), 
+                            rs.getString(4),
+                            rs.getInt(5));                
+                }
+            }
+        }
+        return list;
     }
 
 }
