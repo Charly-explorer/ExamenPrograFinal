33a792c1944e1303b908b3702057b3b5667d60b4
Charly <cagonzalezob@est.utn.ac.cr>
2024-11-27 16:47:22 -0600

    nigga

--- Controlador/Controlador.java
+++ Controlador/Controlador.java
@@ -1,13 +1,33 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Controlador;
+
+import Model.Inscripcion.InscripcionesDAO;
+import Model.Participantes.Participante;
+import Model.Participantes.ParticipanteDao;
+import Model.Participantes.ParticipanteMapper;
+import java.sql.SQLException;
 
 /**
  *
  * @author Student
  */
 public class Controlador {
+    private InscripcionesDAO dao;
+    private ParticipanteDao pardao;
+    private ParticipanteMapper parMapper;
+
+    public Controlador(InscripcionesDAO dao, ParticipanteDao pardao, ParticipanteMapper parMapper) {
+        this.dao = dao;
+        this.pardao = pardao;
+        this.parMapper = parMapper;
+    }
     
+    
+    public Participante buscarParticipante(String id) throws SQLException{
+      if(id==null){
+          return null;
+      }  
+ 
+         return parMapper.toEnt(pardao.leer(id));
+      
+ }
 }
